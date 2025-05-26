4e25142e5083120cf6253db3ddebc91638536f05
FrankieFong-hk <httfong@gmail.com>
2025-05-26 10:41:36 -0400

    feat: implement analytics dashboard with cards and sales chart visualization

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,3 +1,7 @@
+import User from "../models/user.model.js";
+import Product from "../models/product.model.js";
+import Order from "../models/order.model.js";
+
 export const getAnalytics = async (req, res) => {
   try {
     const analyticsData = await getAnalyticsData();
